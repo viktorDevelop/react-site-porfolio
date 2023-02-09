49b8e108dce71e16bad38eee8b477a201202ce5c
viktor <vk.job.00@mail.ru>
2023-02-09 22:56:46 +0300

    add page home

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import './css/main.css';
-
-
-
-
 import Navbar from './components/navbar/Navbar';
-import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Home from './pages/home';
 function App() {
   return (
     <div className="App">
         
-    <Navbar/>
-   
-    <Header/>
-    <main className="section">
-        <div className="container">
-
-                <ul className="content-list">
-                    <li className="content-list__item">
-                        <h2 className="title-2">Frontend</h2>
-                        <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
-                    </li>
-                    <li className="content-list__item">
-                        <h2 className="title-2">Backend</h2>
-                        <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-                    </li>
-                </ul>
-
-        </div>
-    </main>
-
-
+     <Navbar/>
+      <Home/>
       <Footer></Footer>
     </div>
   );
