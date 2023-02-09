7e8efee322f33cf37420885277267a8981eba690
viktor <vk.job.00@mail.ru>
2023-02-09 22:39:41 +0300

    add components header,footer in app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import './css/main.css';
 
 
-import vk from './img/icons/vk.svg';
-import linkedIn from './img/icons/linkedIn.svg';
-import gitHub from './img/icons/gitHub.svg';
-import twitter from './img/icons/twitter.svg';
-import instagram from './img/icons/instagram.svg';
+
 
 import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
         
-    <Navbar></Navbar>
-    <header className="header">
-        <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Yuri</em></strong><br/>
-                a frontend developer
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href="#!" className="btn">Download CV</a>
-        </div>
-    </header>
-
+    <Navbar/>
+   
+    <Header/>
     <main className="section">
         <div className="container">
 
@@ -43,23 +30,8 @@ function App() {
         </div>
     </main>
 
-    <footer className="footer">
-        <div className="container">
-            <div className="footer__wrapper">
-                <ul className="social">
-                    <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={twitter} alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
-                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
-                </ul>
-                <div className="copyright">
-                    <p>© 2022 frontend-dev.com</p>
-                </div>
-            </div>
-        </div>
-    </footer>
 
+      <Footer></Footer>
     </div>
   );
 }
