2ce2bb3322b23b3db7a855e15d70f4b9a3983886
viktor <vk.job.00@mail.ru>
2023-02-11 09:06:55 +0300

    project data

---
+++ src/helpers/projectsList.js
@@ -0,0 +1,47 @@
+const projectsList = [
+
+    {
+        id:1,
+        title:'Gaming streaming portal',
+        image:'01.jpg',
+        skils:'nodejs,mongo,react'
+        
+    },
+    {
+        id:2,
+        title:'Video service',
+        image:'02.jpg',
+        skils:'nodejs,mongo,react'
+        
+    },
+    {
+        id:3,
+        title:'Video portal',
+        image:'03.jpg',
+        skils:'nodejs,mongo,react'
+        
+    },
+    {
+        id:4,
+        title:'Dating app',
+        image:'04.jpg',
+        skils:'nodejs,mongo,react'
+        
+    },
+    {
+        id:5,
+        title:'Landing',
+        image:'05.jpg',
+        skils:'nodejs,mongo,react'
+        
+    },
+    {
+        id:6,
+        title:'Gaming community',
+        image:'06.jpg',
+        skils:'nodejs,mongo,react'
+        
+    }
+];
+
+export default projectsList;
