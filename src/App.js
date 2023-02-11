f52335fd98301bbbe62f5604fd3967f8dba16b2d
viktor <vk.job.00@mail.ru>
2023-02-11 09:11:04 +0300

    app, index.js fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './css/main.css';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Projects from './pages/projects';  
 import Home from './pages/home';
+import Contacts from './pages/contacts';
+import projectDetail from './pages/projectDetail';
 function App() {
   return (
     <div className="App">
         
      <Navbar/>
-      <Home/>
-      <Footer></Footer>
+      {/* <Home/> */}
+      {/* <Projects/> */}
+      {projectDetail}
+      {/* <Contacts/> */}
+      <Footer/>
     </div>
   );
 }
