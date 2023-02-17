da85fad890e55c3e839c587992437d0e4e8eb8e8
viktor <vk.job.00@mail.ru>
2023-02-17 12:18:20 +0300

    fix app

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Routes>
             <Route  path='/' element={<Home/>} /> 
             <Route  path='/projects' element={<Projects/>} /> 
-            <Route  path='/' element={<ProjectDetail/>} /> 
+            <Route  path='/projects/:id' element={<ProjectDetail/>} /> 
             <Route  path='/contacts' element={<Contacts/>}  /> 
                          
 
