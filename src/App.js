5e087b2ed7387d73429de634e017f47d48b20e9d
viktor <vk.job.00@mail.ru>
2023-02-18 11:27:20 +0300

    утилита ScrollToTop

--- src/App.js
+++ src/App.js
@@ -7,27 +7,24 @@ import Contacts from './pages/contacts';
 import ProjectDetail from './pages/ProjectDetail';
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
        <Router>
+        <ScrollToTop/>
         <Navbar/>
-
         <Routes>
+       
             <Route  path='/' element={<Home/>} /> 
             <Route  path='/projects' element={<Projects/>} /> 
             <Route  path='/project/:id' element={<ProjectDetail/>} /> 
             <Route  path='/contacts' element={<Contacts/>}  /> 
-                         
-
             
         </Routes>
-          {/* <Home/> */}
-          {/* <Projects/> */}
-          {/* {<ProjectDetail />} */}
-          {/* <Contacts/> */}
-          <Footer/>
+          
+        <Footer/>
       </Router>
       
     </div>
